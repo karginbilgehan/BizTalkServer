4bec6ef6ead23147274f9b19b535c5f4e9ae3b07
tarikkilic <33003079+tarikkilic@users.noreply.github.com>
2018-12-26 18:24:49 +0300

    approve servis eklendi

---
+++ src/Services/Approve/ApproveService.java
@@ -0,0 +1,90 @@
+package com.server.approve;
+
+import DB.DBHandler;
+import DB.Job;
+import DB.Rule;
+
+import javax.jws.WebMethod;
+import javax.jws.WebService;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.ws.Endpoint;
+import java.util.Arrays;
+import java.util.List;
+
+@WebService()
+public class ApproveService {
+  String userApp, relativeId;
+  Integer jobId;
+
+  @WebMethod()
+  public String updateUserApprove(@XmlElement(required = true, nillable = false) com.server.approve.requestApprove ra){
+
+    //guiden gelen degerler.
+    userApp = ra.getUserApprove();
+    jobId = ra.getJobId();
+    relativeId = ra.getRelativeId().toString();
+    DBHandler db = new DBHandler();
+    //guiden gelen jobid ile db deki job bilgilerine ulasiyoruz.
+    Job job = null;
+    try {
+      job = db.getJob(jobId);
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+
+    //o jobun relativeleri
+    String relatives = job.getRelatives();
+    System.out.println(relatives);
+
+    //bize gelen relativeId nin hangi sırada oldugunu buluyoruz.
+    //db den "x|x|x" şeklinde string geliyor ve bize gelen relatiiveId hangi sırada onu buluyoruz.
+    //Cunku db deki rule tablosunda ki relativeStatus "x,x,x"  stringini buldugumuz indexi degistiyoruz.
+    //Cevap ise guiden gelen userApprove
+
+    //Bize gelen stringi listeye atip indexini buldum.
+    // | ve bosluklari yok sayarak
+    List<String> reList = Arrays.asList(relatives.split("\\s*\\|\\s*"));
+    //try catch eklenecek
+    int index = reList.indexOf(relativeId);
+    //job tablosundan ruleid ye ulasip oradan rule tablosuna erisiyoruz.
+    int ruleId = job.getRuleId();
+    System.out.println(ruleId);
+    Rule rule = null;
+    try {
+      rule = db.getRule(ruleId);
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+
+    //Relativeresult i , ve bosluklari yok sayarak listeye attim.
+    //yukarida buldugum indexteki yere userApprove ile degistirdim listede.
+    String relativeRes = rule.getRelativeResults();
+    System.out.println(relativeRes);
+    List<String> resList = Arrays.asList(relativeRes.split("\\s*,\\s*"));
+    resList.set(index,userApp);
+
+
+    //listeyi tekrar stringe cevirdim virgulle ayrilmis sekilde.
+    StringBuilder strBuilder = new StringBuilder();
+    for(int i = 0; i < resList.size();i++){
+      if(i == resList.size() -1){
+        strBuilder.append(resList.get(i));
+      }
+      else{
+        strBuilder.append(resList.get(i) + ",");
+      }
+    }
+    String newRelative = strBuilder.toString();
+
+    //En son olarak rule tablosunu guncelledim.
+    try {
+      db.updateRule(ruleId,"RelativeResult",newRelative);
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+    System.out.println(newRelative);
+
+    return "update relative";
+  }
+
+}
---
+++ src/Services/Approve/requestApprove.java
@@ -0,0 +1,34 @@
+package com.server.approve;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+import java.io.Serializable;
+
+
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlRootElement(name = "MyRequestApprove", namespace="http://your.namespace.com")
+public class requestApprove implements Serializable{
+
+    @XmlElement(name = "userApprove", required = true)
+    protected String userApprove;
+    @XmlElement(name = "jobId", required = true)
+    protected Integer jobId;
+    @XmlElement(name = "relativeId", required = true)
+    protected  Integer relativeId;
+
+
+
+    public String getUserApprove() {
+        return userApprove;
+    }
+
+    public Integer getJobId() {
+        return jobId;
+    }
+
+    public Integer getRelativeId() {
+        return relativeId;
+    }
+}
