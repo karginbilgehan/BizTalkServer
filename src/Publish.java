fb53f4885b053d32f4eaf78965240029dfe194a9
optimur <onurhis@gmail.com>
2018-12-26 14:32:25 +0300

    Kendi bilgisayarınızda test etmek için, password değişkenini kendi local mysql şifreniz ile doldurunuz.

--- src/Publish.java
+++ src/Publish.java
@@ -3,16 +3,13 @@ import DB.Job;
 import Services.Orchestration.OrchestrationService;
 
 import javax.xml.ws.Endpoint;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.Date;
 
 public class Publish {
 
     public static void main(String[] argv) throws Exception {
         Object implementor = new OrchestrationService();
         String address = "http://localhost:9001/OrchestrationService";
-        Endpoint.publish(address, implementor);
+        //Endpoint.publish(address, implementor);
 
         DBHandler dbHandler = new DBHandler();
         Job job = new Job(151044083,"Açıklama yazısıdır.","10.0.0.1,10.0.0.2,10.0.0.3","http://www.cevkos.gtu.edu.tr/wp-content/uploads/2018/03/GTU_sponsor-1.png","1510,1511,1512",0,123);
