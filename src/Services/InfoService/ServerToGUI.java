811f91d136bedb3e9cafc677db9c6925a1969ba3
Ferhat Şirin <ferhat_sirn@hotmail.com>
2018-12-26 17:26:28 +0300

    Create ServerToGUI.java

---
+++ src/Services/InfoService/ServerToGUI.java
@@ -0,0 +1,56 @@
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlRootElement;
+
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlRootElement(name ="ServerToGUI", namespace="Server")
+public class ServerToGUI {
+
+    protected Integer jobId;
+    protected Integer jobOwner;
+    protected String jobDescription;
+    protected String relatives;
+    protected Integer status;
+
+    public Integer getJobId() {
+        return jobId;
+    }
+
+    public Integer getJobOwner() {
+        return jobOwner;
+    }
+
+    public String getJobDescription() {
+        return jobDescription;
+    }
+
+    public String getRelatives() {
+        return relatives;
+    }
+
+    public Integer getStatus() {
+        return status;
+    }
+
+    public void setJobId(Integer jobId) {
+        this.jobId = jobId;
+    }
+
+    public void setJobOwner(Integer jobOwner) {
+        this.jobOwner = jobOwner;
+    }
+
+    public void setJobDescription(String jobDescription) {
+        this.jobDescription = jobDescription;
+    }
+
+    public void setRelatives(String relatives) {
+        this.relatives = relatives;
+    }
+
+    public void setStatus(Integer status) {
+        this.status = status;
+    }
+
+}
