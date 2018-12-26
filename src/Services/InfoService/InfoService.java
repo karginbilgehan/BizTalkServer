2dfcf92f5ed71dd054a28095797f62232c071eaa
Ferhat Şirin <ferhat_sirn@hotmail.com>
2018-12-26 16:46:57 +0300

    Update InfoService.java

--- src/Services/InfoService/InfoService.java
+++ src/Services/InfoService/InfoService.java
@@ -10,12 +10,32 @@ import javax.xml.bind.annotation.XmlElement;
 @WebService(serviceName = "InfoService")
 public class InfoService {
 
-    @WebMethod
-    public Job getjob(@XmlElement(required = true, nillable = false)  Integer jobId)throws Exception {
+ @WebMethod
+    public Job getJob(Integer jobId) throws Exception{
+
         DBHandler handler =new DBHandler();
         Job job =handler.getJob(jobId.intValue());
-
+        
         return job;
     }
+    
+    @WebMethod
+    public Set<Orchestration> getOrchestration(Integer ownerId) throws Exception{
+    
+        DBHandler handler =new DBHandler();
+        Set<Orchestration> orc =handler.getOrchestration(ownerId.intValue());
+
+        return orc;
+    }
+    
+    @WebMethod
+    public Rule getRule(Integer ruleId) throws Exception{
+    
+        DBHandler handler =new DBHandler();
+        Rule rule =handler.getRule(ruleId.intValue());
+        
+        return rule;
+    }
+
 
 }
