ebac8071a3aaa7db6da2d9327bc70bca386aa63e
Yusuf Akdas <yusufakdas@gtu.edu.tr>
2019-01-03 19:11:48 +0300

    Publish.java updated

--- src/MainProcess/Publish.java
+++ src/MainProcess/Publish.java
@@ -6,29 +6,43 @@ import Services.InfoService.InfoService;
 import Services.Orchestration.OrchestrationService;
 
 import javax.xml.ws.Endpoint;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.net.SocketException;
+import java.net.UnknownHostException;
 
 public class Publish {
 
+    private static String getHostIp(){
+        String ip="";
+        try(final DatagramSocket socket = new DatagramSocket()){
+            socket.connect(InetAddress.getByName("8.8.8.8"), 10002);
+            ip = socket.getLocalAddress().getHostAddress();
+        } catch (UnknownHostException | SocketException e) {
+            e.printStackTrace();
+        }
+        return ip;
+    }
+
     private static void startOrchestrationService() {
         Object implementor = new OrchestrationService();
-        String address = "http://localhost:9001/OrchestrationService";
+        String address = "http://" + getHostIp() + ":9001/OrchestrationService";
         Endpoint.publish(address, implementor);
     }
 
     private static void startInfoService() {
         Object implementor = new InfoService();
-        String address = "http://localhost:9001/InfoService";
+        String address = "http://" + getHostIp() + ":9001/InfoService";
         Endpoint.publish(address, implementor);
     }
 
     private static void startApproveService() {
         Object implementor = new ApproveService();
-        String address = "http://localhost:9001/ApproveService";
+        String address = "http://" + getHostIp() + ":9001/ApproveService";
         Endpoint.publish(address, implementor);
     }
 
     public static void main(String[] argv) {
-
         startInfoService();
         startOrchestrationService();
         startApproveService();
